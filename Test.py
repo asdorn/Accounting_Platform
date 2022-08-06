02c7608aa011ce2181195259ce13fe633ff2c0a2
AsafDorin <92961748+asdorn@users.noreply.github.com>
2022-08-07 01:44:52 +0300

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,15 @@
+from Class_Automated_Accountant import Accountant
+
+company = Accountant()
+
+company.create_balance()
+
+company.show_balance()
+
+company.issue()
+
+company.buying_permanent_possesion()
+
+company.finance_proportion()
+
+company.show_balance()
