c3115bc19e5bac9f99aa01bf926960d1da4c5937
AsafDorin <92961748+asdorn@users.noreply.github.com>
2022-08-07 01:42:52 +0300

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,15 @@
+from Develop import Accountant
+
+company = Accountant()
+
+company.create_balance()
+
+company.show_balance()
+
+company.issue()
+
+company.buying_permanent_possesion()
+
+company.finance_proportion()
+
+company.show_balance()
